3484e4f4b7999691093ae9012199776c81b7d617
Sajib <sajibdashtrg@gmail.com>
2025-10-05 23:43:41 +0600

    first commit

---
+++ script .js
@@ -0,0 +1,7 @@
+ // Toggle mobile menu
+    const menuBtn = document.getElementById('menu-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuBtn.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
